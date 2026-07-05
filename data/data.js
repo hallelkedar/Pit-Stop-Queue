776019667ade4d304731b5296179fcc1b90ae632
Hallel <hallelkedar000@gmail.com>
2026-07-05 10:35:44 +0300

    Added io functions, fetch, save and read

--- data/data.js
+++ data/data.js
@@ -1,5 +1,38 @@
-const APIURL = 'https://server-for-test-week-13.onrender.com/api/race'
+import { log } from "console";
+import fs from "fs/promises";
+import { waitForDebugger } from "inspector";
 
-async function name(params) {
+const APIURL = "https://server-for-test-week-13.onrender.com/api/race";
+const DATAPATH = "race.json";
+
+async function loadServerData() {
+  try {
+    const res = await fetch(APIURL);
+    if (!res.ok) throw new Error(res.status);
+    const data = await res.json()
+    await saveToFile(data)
+    return true
     
+  } catch (err) {
+    return err.message;
+  }
 }
+
+async function saveToFile(obj) {
+  try {
+    await fs.writeFile(DATAPATH, JSON.stringify(obj, null, 2), 'utf-8');
+    
+  } catch (err) {
+    return err.message;
+  }
+}
+
+async function getData() {
+    try {
+    const data =  await fs.readFile(DATAPATH, 'utf-8')
+    return JSON.parse(data)
+    }
+    catch (err) {
+        return err.message
+    }
+}   
